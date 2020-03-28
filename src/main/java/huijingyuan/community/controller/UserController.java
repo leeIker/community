21b652edb25117484ce5fca6b41d3fc80ab7016f
cheeser <649457551@qq.com>
2020-03-28 14:29:53 +0800

    给controller类加注解 使其能够跨域请求

--- src/main/java/huijingyuan/community/controller/UserController.java
+++ src/main/java/huijingyuan/community/controller/UserController.java
@@ -6,13 +6,14 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import huijingyuan.community.dao.UserDao;
 import huijingyuan.community.dto.UserDto;
 import huijingyuan.community.service.UserService;
-
+@CrossOrigin
 @Controller
 @RequestMapping("user")
 public class UserController {
